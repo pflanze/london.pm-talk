169ca5591a560c6b9417835fd68b8a44489085c7
Christian Jaeger <ch@christianjaeger.ch>
2015-05-31 20:51:30 +0200

    make `pretty-quotes` actually work

--- pretty-quotes.scm
+++ pretty-quotes.scm
@@ -3,14 +3,26 @@
 				  ;; that treats words of most human
 				  ;; languages correctly.
 
-(def (non-word? v)
-     (or (not v)
-	 (not (char-word? v))))
+(def word-char? (both char? char-word?))
+(def whitespace-char? (both char? char-whitespace?))
+
+(def. (false.pretty-order v)
+  0)
+
+(def. (char.pretty-order v)
+  2)
+
+(def. (whitespace-char.pretty-order v)
+  1)
+
+(def. (word-char.pretty-order v)
+  3)
 
 (def (maybe-car l)
      (if (null? l)
 	 #f
 	 (car l)))
+
 
 (def. (string.first s)
   (string-ref s 0))
@@ -27,17 +39,43 @@
 (def pretty-opening-singlequote (.first "‘"))
 (def pretty-closing-singlequote (.first "’"))
 
+
+(def pretty-order-cmp
+     (on .pretty-order number-cmp))
+
+(TEST
+ > (.pretty-order #\.)
+ 2
+ > (.pretty-order #\0)
+ 2
+ > (.pretty-order #\space)
+ 1
+ > (.pretty-order #\a)
+ 3
+ > (pretty-order-cmp #\0 #\a)
+ lt
+ > (pretty-order-cmp #\0 #f)
+ gt
+ > (pretty-order-cmp #f #\0)
+ lt
+ > (pretty-order-cmp #\a #\0)
+ gt
+ > (pretty-order-cmp #\a #\.)
+ gt
+ > (pretty-order-cmp #\. #\space)
+ gt
+ )
+
+
 (def (list-pretty-quotes l #!optional maybe-prev-c)
      (if (pair? l)
 	 (let-pair ((c l*) l)
 		   (def (choose opening closing)
-			(if (non-word? maybe-prev-c)
-			    (if (non-word? (maybe-car l*))
-				c
-				opening)
-			    (if (non-word? (maybe-car l*))
-				closing
-				c)))
+			(match-cmp
+			 (pretty-order-cmp maybe-prev-c (maybe-car l*))
+			 ((lt) opening)
+			 ((eq) c)
+			 ((gt) closing)))
 		   (cons (case c
 			   ((#\")
 			    (choose pretty-opening-doublequote
@@ -70,5 +108,8 @@
  > (pretty-quotes "You are 'nice'.")
  "You are ‘nice’."
  > (pretty-quotes "\"Always\" is a bit strong.")
- "“Always” is a bit strong.")
+ "“Always” is a bit strong."
+ > (pretty-quotes "\"Always.\" ")
+ "“Always.” "
+ )
 
